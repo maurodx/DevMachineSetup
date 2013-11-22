6e6b4c98b2e41dbf3de93708a2994729dd25369b
Mauro Destro <maurodx@email.it>
2013-11-22 12:25:45 +0100

    Update BootIt.ps1

--- Bootstrap/BootIt.ps1
+++ Bootstrap/BootIt.ps1
@@ -84,6 +84,7 @@ if($helperDir.Count -gt 1){$helperDir = $helperDir[-1]}
 import-module $helperDir\boxstarter.helpers.psm1
 
 Set-ExplorerOptions -showHidenFilesFoldersDrives -showProtectedOSFiles -showFileExtensions
+Enable-RemoteDesktop
 
 Install-ChocolateyPinnedTaskBarItem (Find-Program "Google\Chrome\Application\chrome.exe")
 Install-ChocolateyPinnedTaskBarItem "$env:SystemRoot\system32\WindowsPowerShell\v1.0\powershell.exe"
@@ -91,7 +92,7 @@ Install-ChocolateyPinnedTaskBarItem "$env:SystemRoot\system32\WindowsPowerShell\
 Install-ChocolateyFileAssociation ".txt" "$editorOfChoice"
 Install-ChocolateyFileAssociation ".dll" "$env:ChocolateyInstall\bin\dotPeek.bat"
 
-
+Install-WindowsUpdate -AcceptEulaInstall-WindowsUpdate -AcceptEula
 
 
 
